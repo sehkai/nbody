ba6c995508370005ad8e245bde98070d1ecfdf66
Tim Padjen <tpadjen@gmail.com>
2012-01-06 02:30:14 -0700

    Added NBody class with list of simulations in main

---
+++ NBody.java
@@ -0,0 +1,39 @@
+public class NBody {
+	
+	public static void main(String[] args) {
+		// Universe uni = new Universe("3body.txt");
+		// Universe uni = new Universe("8star-rotation.txt");
+		// Universe uni = new Universe("antimatter.txt");
+		// Universe uni = new Universe("armageddon.txt");
+		// Universe uni = new Universe("atom.txt");
+		// Universe uni = new Universe("awesome.txt");
+		// Universe uni = new Universe("binary.txt");
+		// Universe uni = new Universe("binaryStars.txt");
+		// Universe uni = new Universe("chaosblossom.txt");
+		// Universe uni = new Universe("dance10.txt");
+		// Universe uni = new Universe("entroy-universe.txt");
+		// Universe uni = new Universe("fourellipses.txt");
+		// Universe uni = new Universe("galaxy.txt");
+		// Universe uni = new Universe("hypnosis.txt");
+		// Universe uni = new Universe("illusion.txt");
+		// Universe uni = new Universe("its-a-trap.txt");
+		// Universe uni = new Universe("kaleidoscope.txt");
+		// Universe uni = new Universe("kevin.txt");
+		// Universe uni = new Universe("massive-squirrel-battle.txt");
+		// Universe uni = new Universe("planets-elliptical.txt");
+		Universe uni = new Universe("planets.txt");
+		// Universe uni = new Universe("planetsparty.txt");
+		// Universe uni = new Universe("pluto.txt");
+		// Universe uni = new Universe("quad-stars.txt");
+		// Universe uni = new Universe("renegade.txt");
+		// Universe uni = new Universe("sbh2.txt");
+		// Universe uni = new Universe("sbh3.txt");
+		// Universe uni = new Universe("soap-opera.txt");
+		// Universe uni = new Universe("spiral.txt");
+		// Universe uni = new Universe("suninterference.txt");
+		// Universe uni = new Universe("twin-binaries.txt");
+		// Universe uni = new Universe("uniform100.txt");
+		// Universe uni = new Universe("uniform3.txt");
+		// Universe uni = new Universe("uniform8.txt");
+	}
+}
